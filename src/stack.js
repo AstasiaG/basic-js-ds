c523dbaa9911bcf25b430d71b8e1dfdcb0cf9d37
Anastasia <anstasa.li@gmail.com>
2025-01-07 00:45:33 +0300

    feat: add stack task

--- src/stack.js
+++ src/stack.js
@@ -13,18 +13,23 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class Stack {
-  push(/* element */) {
-    throw new NotImplementedError('Not implemented');
+  stack = [];
+  push(element) {
+    this.stack.push(element);
+    return this;
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   pop() {
-    throw new NotImplementedError('Not implemented');
+    return this.stack.pop();
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 
   peek() {
-    throw new NotImplementedError('Not implemented');
+    return this.stack[this.stack.length - 1]
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
   }
 }
